fedf3e23834bc65c6340e67d6b5269c9275a2c50
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-09 22:54:38 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,16 +1,22 @@
 #include<string.h>
 #include<stdlib.h>
 
-int add (const char* input)
+int Checkforempty (const char *input)
 {
     int stringlen = strlen(input);
-  
+    
     if( stringlen == 0)
-      return 0;
+      return 0;  
+}
 
+int CompareforZero (const char *input)
+{
     if (strcmp(input, "0") == 0)
       return 0;
+}
 
+int SumofTwoNumbers (const char *input)
+{
     const char *comma_pos = strchr(input, ',');
     if ( comma_pos != NULL)
     {
@@ -19,6 +25,13 @@ int add (const char* input)
 
         return num1+num2;
     }
+}
 
+int add (const char* input)
+{
+    Checkforempty(input);
+    CompareforZero(input);
+    SumofTwoNumbers(input);
+    
     return -1;
 }
